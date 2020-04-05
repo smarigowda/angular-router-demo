5cb143c3b12fa1b124949e75609f73733fb9d0ff
Santosh Marigowda <santosharakere@gmail.com>
2020-04-05 13:58:08 +0100

    Angular Router Demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
